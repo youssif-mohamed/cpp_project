1eb5cd293533da4e0f7322c7c1a6f1543e91ab0f
Youssif Mohamed <s-youssifmohamed@zewailcity.edu.eg>
2021-06-15 13:57:46 +0200

    Add status data field to Client class

--- Client.h
+++ Client.h
@@ -7,11 +7,11 @@ using namespace std;
 class Client
 {
 private:
-    int start, end;
+    int start, end,status; // if status == 0 so the client is not in the elvator elif status == 1 so in the elvator elif == 2 so arrived to destination
     char dir;
 public:
     Client();
-    Client(int, int, char);
+    Client(int, int, char, int);
     ~Client();
     void setStart(int);
     void setEnd(int);
@@ -19,6 +19,8 @@ public:
     int getStart();
     int getEnd();
     char getDir();
+    int getStatus();
+    void setStatus(int);
 
 };
 
@@ -27,11 +29,13 @@ Client::Client()
     start = 2;
     end = 2;
     dir = 'U';
+    status = 0;
 }
-Client::Client(int s, int e, char d){
+Client::Client(int s, int e, char d, int st){   // s, e, d, st are dummy variables and theur scope is just in this function 
     start = s;
     end = e;
     dir = d;
+    status = st;
 }
 // set member start 
 void Client::setStart(int s){
@@ -51,6 +55,12 @@ int Client::getEnd(){
 }
 char Client::getDir(){
     return dir;
+}
+int Client::getStatus(){
+    return status;
+}
+void Client::setStatus(int st){
+    status = st;
 }
 Client::~Client()
 {
